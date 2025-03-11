03a8f8209e2505d37df0fb9ee67838b5e23a54d3
Zack <zardragos@gmail.com>
2025-03-11 12:44:19 -0400

    added userpass to env variables

--- CADWebsite/submit_form.php
+++ CADWebsite/submit_form.php
@@ -1,39 +1,43 @@
 <?php
 header('Content-Type: application/json');
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
+
+// Load environment variables
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
-require "vendor/autoload.php";
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
 $mail = new PHPMailer(true);
 
-//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-$mail->Username = "de.designs.autoemail@gmail.com";
-$mail->Password = "sjlp qfdk gtfc gupk";
-
-$mail->setFrom($email, $name);
-$mail->addAddress("zdragos.work@gmail.com", "Zack");
-
-$mail->Subject = $subject;
-$mail->Body = $message;
-
-$mail->send();
-
-
 try {
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    // Use environment variables
+    $mail->Username = $_ENV["SMTP_USERNAME"];
+    $mail->Password = $_ENV["SMTP_PASSWORD"];
+
+    $mail->setFrom($email, $name);
+    $mail->addAddress("JackCeriello@gmail.com", "Zack");
+
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+
+    $mail->SMTPDebug = 0;
+
     $mail->send();
     echo json_encode(["status" => "success"]);
 } catch (Exception $e) {
