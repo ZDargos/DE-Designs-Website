0da0e1cbb1cae1309173b743adb4c2599ade8b1f
Zack Dragos <zardragos@gmail.com>
2025-03-11 11:30:09 -0400

    php bug fixes

--- CADWebsite/submit_form.php
+++ CADWebsite/submit_form.php
@@ -1,8 +1,8 @@
 <?php
-$name = htmlspecialchars($_POST["name"]);
-$email = htmlspecialchars($_POST["email"]);
-$subject = htmlspecialchars($_POST["subject"]);
-$message = htmlspecialchars($_POST["message"]);
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 require "vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
