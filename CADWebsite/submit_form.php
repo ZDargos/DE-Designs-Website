fffa47323535814638a4ca83f17b1e1b38cec9f6
Zack Dragos <zardragos@gmail.com>
2025-03-11 11:24:25 -0400

    removed if statement in php

--- CADWebsite/submit_form.php
+++ CADWebsite/submit_form.php
@@ -1,32 +1,30 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $subject = htmlspecialchars($_POST["subject"]);
-    $message = htmlspecialchars($_POST["message"]);
-    require "vendor/autoload.php";
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$subject = htmlspecialchars($_POST["subject"]);
+$message = htmlspecialchars($_POST["message"]);
+require "vendor/autoload.php";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-    $mail = new PHPMailer(true);
+$mail = new PHPMailer(true);
 
-    $mail->isSMTP();
-    $mail->SMTPAuth = true;
+$mail->isSMTP();
+$mail->SMTPAuth = true;
 
-    $mail->Host = "de.designs.autoemail@gmail.com";
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+$mail->Host = "de.designs.autoemail@gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
 
-    $mail->Username = "de.designs.autoemail@gmail.com";
-    $mail->Password = "jackspassword";
+$mail->Username = "de.designs.autoemail@gmail.com";
+$mail->Password = "jackspassword";
 
-    $mail->setFrom($email, $name);
-    $mail->addAddress("zardragos@gmail.com", "Zack");
+$mail->setFrom($email, $name);
+$mail->addAddress("zardragos@gmail.com", "Zack");
 
-    $mail->Subject = $subject;
-    $mail->Body = $message;
+$mail->Subject = $subject;
+$mail->Body = $message;
 
-    $mail->send();
+$mail->send();
 
-    header("Location: sent.html");
-}
+header("Location: sent.html");
